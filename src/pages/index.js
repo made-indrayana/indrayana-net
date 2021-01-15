f614118fec99d43518e4449f21673deaaf6646d3
Made Indrayana <indra@doubleshot-audio.com>
2021-01-15 23:50:56 +0100

    Finishing gatsbyjs Tutorial Part One :D

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
 import React from "react"
+import {Link} from "gatsby"
+import Layout from '../components/layout'
 
 export default function Home() {
-  return <div>This is the beginning of indrayana.net!</div>
+  return <div style={{ color: `purple`}}>
+    <Layout>
+    <p>Now: Hello World :)</p>
+    <img src="https://source.unsplash.com/random/400x200" alt="" /><br />
+    <button class="primary-button">Click me</button><br />
+    <Link to='/about'>About me</Link>
+    </Layout>
+    </div>
 }
