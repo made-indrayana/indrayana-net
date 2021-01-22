71be31f30e185cf2a61cfa8b5bc14300f8cc38b1
Made Indrayana <indra@doubleshot-audio.com>
2021-01-22 05:24:58 +0100

    Tutorial part two done

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,13 @@
 import React from "react"
+import Container from "../components/container"
 import Layout from '../components/layout'
 
 export default function Blog() {
   return (
     <Layout>
+      <Container>
       <p>Such wow. Very React.</p>
+      </Container>
     </Layout>
   )
 }
