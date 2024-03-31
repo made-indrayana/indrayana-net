26378cc0a90e48db7217a35408e8cd30ceca2fa8
Made Indrayana <indra@indrayana.net>
2024-03-31 21:33:39 +0200

    Separating JS from main HTML - small changes here and there

---
+++ script.js
@@ -0,0 +1,48 @@
+// On Page Load & Button Clicks
+window.addEventListener("load", function () {
+  document.getElementById("mark").classList.add("mark-transition");
+  this.setTimeout(function () {
+    document.getElementById("mark").classList.remove("mark-transition");
+  }, 1000);
+});
+
+document.getElementById("github-button").addEventListener("click", function () {
+  window.open("https://github.com/made-indrayana", "_blank");
+});
+
+document
+  .getElementById("linkedin-button")
+  .addEventListener("click", function () {
+    window.open("https://www.linkedin.com/in/made-indrayana/", "_blank");
+  });
+
+// Cube Scripts
+const container = document.documentElement;
+const cube = document.getElementById("cube");
+
+container.addEventListener("mousemove", (e) => {
+  const mouseX = e.clientX - container.offsetLeft;
+  const mouseY = e.clientY - container.offsetTop;
+
+  const rotateY = (mouseX / window.innerWidth - 0.5) * 100;
+  const rotateX = (mouseY / window.innerHeight - 0.5) * -100;
+
+  cube.style.transform = `rotateY(${rotateY}deg) rotateX(${rotateX}deg)`;
+});
+
+// Blink function
+let animationSpeed = 500;
+
+document.addEventListener("mousedown", function (event) {
+  if (event.button === 0) {
+    // Check if the left mouse button is clicked (button code: 0)
+
+    const logoFace = document.querySelector(".logo");
+    logoFace.classList.add("blink");
+
+    // After a short delay, remove the 'blink' class to stop the blinking effect
+    setTimeout(function () {
+      logoFace.classList.remove("blink");
+    }, animationSpeed);
+  }
+});
